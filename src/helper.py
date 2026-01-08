ca08a51f57bcd6e956a9df77d47d9ad2bf2363b5
VVUNitian <bibhupb2004@gmail.com>
2026-01-09 01:41:24 +0530

    Add newline at end of file in helper.py

--- src/helper.py
+++ src/helper.py
@@ -37,3 +37,4 @@ def text_split(extracted_data):
 def download_hugging_face_embeddings():
     embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
     return embeddings
+
